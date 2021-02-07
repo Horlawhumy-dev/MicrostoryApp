3c1550d641eae0e78e9bfec2e9759f72600ee74d
Arafat Olayiwola <harof.dev@gmail.com>
2021-02-07 04:20:04 +0100

    add authentication backends

--- MicrostoryApp/settings.py
+++ MicrostoryApp/settings.py
@@ -107,6 +107,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+    ('django.contrib.auth.backends.ModelBackend'),
+)
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
