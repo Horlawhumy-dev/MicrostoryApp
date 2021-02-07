4dfb30990c90062de42a39dce8045fe4f2d3890a
Arafat Olayiwola <harof.dev@gmail.com>
2021-02-07 02:06:34 +0100

    tweaked settings

--- MicrostoryApp/settings.py
+++ MicrostoryApp/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-p)$+3p3^=z4kzvsz1j+vv$l-s*05=o106ushcadzadd!k(c#='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://microstoryapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
